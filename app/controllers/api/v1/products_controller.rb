53e9d4cf7f23e06058702eff11ffc5869262eef5
renad.shaaban <renad.shaaban@gmail.com>
2017-07-01 15:55:57 +0200

    rendering result in json format

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::ProductsController < ApplicationController
 	
 	def index
 		@products=Product.all
-		#render json: JSON.pretty_generate(Product.index_response), status: 200
+		render json: JSON.pretty_generate(Product.index_response), status: 200
 	end
 end
